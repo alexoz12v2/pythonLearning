31440357cb44ca1449d090e4b4d37149944c9c9c
alexoz12v2 <oboken1974@hotmail.it>
2024-11-22 00:03:21 +0100

    git LFS

--- pytorchLabs/05_PytorchLayerProgramming/houses.py
+++ pytorchLabs/05_PytorchLayerProgramming/houses.py
@@ -1,2 +1,6 @@
 import torch
 from torch import nn
+
+# houses dataset file line example (CSV format with whitespace as separator)
+# numBathrooms numBedrooms houseAreaSqFt HousePriceUSD
+# 4 4 3721 85255 500000
